e230cc31195bd05cf0cb31a4ed4778418e522e2c
nidhijaroli <163496275+nidhijaroli@users.noreply.github.com>
2024-03-15 10:35:05 +0530

    Add files via upload

---
+++ PATTERN.C
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<conio.h>
+  main()
+    {
+	clrscr();
+	printf("hello\n");
+	printf("name:nidhi jaroli\n");
+	printf("school:creative public school\n");
+	printf("age:19\n");
+	printf("-------------------------------------\n");
+	printf("*\n");
+	printf("**\n");
+	printf("***\n");
+	printf("**\n");
+	printf("*\n");
+	printf("---------------------------------------\n");
+	printf(" ----------\n");
+	printf("|          |\n");
+	printf("R          |\n");
+	printf("N          |\n");
+	printf("W          |\n");
+	printf(" ----------\n");
+	printf("-----------------------------------------\n");
+	printf("*\n");
+	printf("*\n");
+	printf("*\n");
+	printf("*          * *        *\n");
+
+
+	getch();
+
+
+
+
+
+  }
